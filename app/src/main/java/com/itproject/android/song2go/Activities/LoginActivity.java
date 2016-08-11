73296cc05cda0c3bf6500423a282fdc669f3c071
EarvinJay <earvinjaybermudo21@gmail.com>
2016-08-11 16:12:44 +0800

    implemeted drawer layout w/o values firebase authentication w/ fb

--- app/src/main/java/com/itproject/android/song2go/Activities/LoginActivity.java
+++ app/src/main/java/com/itproject/android/song2go/Activities/LoginActivity.java
@@ -9,13 +9,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -29,6 +37,8 @@ public class LoginActivity extends AppCompatActivity {
     private LoginButton loginButton;
     Button btnlog;
     private CallbackManager callbackManager;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,14 +46,17 @@ public class LoginActivity extends AppCompatActivity {
 
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
-        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton = (LoginButton)findViewById(R.id.button_facebook_login);
     btnlog= (Button) findViewById(R.id.btnlogin);
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-
+//Toast.makeText(LoginActivity.this,"Success",Toast.LENGTH_SHORT).show();
+  handleFacebookAccessToken(loginResult.getAccessToken());
             }
+
+
 
             @Override
             public void onCancel() {
@@ -52,12 +65,37 @@ public class LoginActivity extends AppCompatActivity {
 
             @Override
             public void onError(FacebookException e) {
-
+                Toast.makeText(LoginActivity.this,"Failed",Toast.LENGTH_SHORT).show();
             }
         });
-
+firebaseAuth=FirebaseAuth.getInstance();
+        firebaseAuthListener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user=firebaseAuth.getCurrentUser();
+                if(user!=null)
+                {
+                    Toast.makeText(LoginActivity.this,"Success",Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
 
     }
+
+    private void handleFacebookAccessToken(AccessToken accessToken) {
+        AuthCredential credential= FacebookAuthProvider.getCredential(accessToken.getToken());
+            firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this,new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+if(!task.isSuccessful())
+{
+    Toast.makeText(LoginActivity.this,"DEAD",Toast.LENGTH_SHORT).show();
+}
+            }
+        });
+    }
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
@@ -68,6 +106,20 @@ public class LoginActivity extends AppCompatActivity {
 
         Intent intent=new Intent(this,MainActivity.class);
         startActivity(intent);
+    }
+
+@Override
+    protected void onStart()
+    {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(firebaseAuthListener);
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(firebaseAuthListener);
     }
 
 
--- app/src/main/java/com/itproject/android/song2go/Activities/MainActivity.java
+++ app/src/main/java/com/itproject/android/song2go/Activities/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.facebook.FacebookSdk;
 import com.itproject.android.song2go.R;
 
 import java.io.File;
@@ -25,51 +26,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lv=(ListView) findViewById(R.id.listView);
+        lv = (ListView) findViewById(R.id.listView);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+//        final ArrayList<File> myVids = findVids(Environment.getExternalStorageDirectory());
+//        Items=new String[myVids.size()];
+//
+//        for(int i=0 ; i < myVids.size() ; i++)
+//        {
+//
+//            Items[i]=myVids.get(i).getName().toString().replace(".mp4","").replace("KAR","").replace(".mid","").replace(".mp3","");
+//        }
+//
+//ArrayAdapter<String> adp= new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,Items);
+//lv.setAdapter(adp);
+//
+//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                startActivity(new Intent(getApplicationContext(),Player.class).putExtra("pos",position).putExtra("videolist",myVids));
+//            }
+//        });
+//    }
+//
+//    public ArrayList<File> findVids(File root)
+//    {
+//        ArrayList<File> arrayl=new ArrayList<File>();
+//        File[] files=root.listFiles();
+//
+//        for(File singlefile : files)
+//        {
+//            if(singlefile.isDirectory() && !singlefile.isHidden())
+//            {
+//   arrayl.addAll(findVids(singlefile));
+//            }
+//            else {
+//            if(singlefile.getName().endsWith(".mp4") || singlefile.getName().endsWith("KAR") || singlefile.getName().endsWith(".mid") || singlefile.getName().endsWith(".mp3")  )
+//                {
+//                    arrayl.add(singlefile);
+//                }
+//            }
+//            }
+//return arrayl;
+//        }
+//    public void toast(String text)
+//    {
+//        Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
+  }
 
-        final ArrayList<File> myVids = findVids(Environment.getExternalStorageDirectory());
-        Items=new String[myVids.size()];
-
-        for(int i=0 ; i < myVids.size() ; i++)
-        {
-
-            Items[i]=myVids.get(i).getName().toString().replace(".mp4","").replace("KAR","").replace(".mid","").replace(".mp3","");
-        }
-
-ArrayAdapter<String> adp= new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,Items);
-lv.setAdapter(adp);
-
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                startActivity(new Intent(getApplicationContext(),Player.class).putExtra("pos",position).putExtra("videolist",myVids));
-            }
-        });
-    }
-
-    public ArrayList<File> findVids(File root)
-    {
-        ArrayList<File> arrayl=new ArrayList<File>();
-        File[] files=root.listFiles();
-
-        for(File singlefile : files)
-        {
-            if(singlefile.isDirectory() && !singlefile.isHidden())
-            {
-   arrayl.addAll(findVids(singlefile));
-            }
-            else {
-            if(singlefile.getName().endsWith(".mp4") || singlefile.getName().endsWith("KAR") || singlefile.getName().endsWith(".mid") || singlefile.getName().endsWith(".mp3")  )
-                {
-                    arrayl.add(singlefile);
-                }
-            }
-            }
-return arrayl;
-        }
-    public void toast(String text)
-    {
-        Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
-    }
-    
         }
